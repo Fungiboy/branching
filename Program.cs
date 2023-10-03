719466852e72e93dd57602b4fda34ed91a157aea
Fungiboy <Lukas.Austrell@gmail.com>
2023-10-03 10:51:31 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// Branches
+// Merges
+// Pull request
+
+// Solo programmering
+// Kod -> git add -> git commit -> git push
+
+//Nytt project (bara en gång)
+// Skapa projekt i VS -> skapa repo i github -> git init -> cp ~/.gitignore.vs.gitignore
+// -> git remote add origin git@<host>:Fungiboy/<projekt namn>.git ->
+// git add . -> git commit -> git push -u origin master   git commit -> git push
+
+/*
+
+Host: github.com
+User: Fungiboy
+
+*/
+
+/*
+origin/master
+
+initial commit
+
+feat: add input
+
+initial input parsing (inte bra)
+*/
+
+/*
+
+manuel/master
+
+initial commit
+
+feat: add input 
+
+feat: initial input parsing (inte bra)
+
+*/
+
+/*
+
+kevin / master
+
+initial commit
+
+feat: add input 
+
+feat: object creation
+
+initial input parsing (inte bra)
+*/
+
+/*
+
+menaid / master
+
+initial commit
+
+feat: add input 
+
+feat: console output
+
+initial input parsing (inte bra)
+
+*/
+
+
+// origin/master -> initial commit -> add input
+// -> git clone ->
+//Fungiboy/master -> initial commit -> add input -> (Fungiboy/input-parsing -> init input parsing -> next input parsing) -> git push -u -> 
+// origin / input parsing -> clone -> Fungiboy /input-parsing -> next input parsing
+
